9e2d8c74f2aba5b98b6f34b2b9ea78448d823a4f
bangc0121 <92088456+bangc0121@users.noreply.github.com>
2023-03-05 13:03:38 +0900

    feature: android scan sdk add

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './src/App'
+import { AppRegistry } from 'react-native'
+import { LogBox } from 'react-native'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+LogBox.ignoreLogs([
+  'Require cycle: node_modules/',
+  'Non-serializable values were found in the navigation state',
+  'Missing queryFn',
+])
+AppRegistry.registerComponent(appName, () => App)
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
 import 'react-native-gesture-handler'
 import './translations'
 
-import { Linking, Platform } from 'react-native'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import React, { useEffect } from 'react'
 import { persistor, store } from '@/store'
 
-import { Amplify } from 'aws-amplify'
 import ApplicationNavigator from '@/navigators/Application'
 import GlobalModal from './components/modal/GlobalModal'
-import InAppBrowser from 'react-native-inappbrowser-reborn'
-import { PersistGate } from 'redux-persist/lib/integration/react'
 import { Provider } from 'react-redux'
 import { RecoilRoot } from 'recoil'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import awsconfig from './aws-exports'
-
-async function urlOpener(url: string | string[], redirectUrl: string) {
-  if (await InAppBrowser.isAvailable()) {
-    if (!url.includes('logout')) {
-      const { type, url: newUrl } = await InAppBrowser.openAuth(
-        url,
-        redirectUrl,
-        {
-          modalEnabled: false,
-          showTitle: false,
-          enableUrlBarHiding: false,
-          enableDefaultShare: false,
-          ephemeralWebSession: false,
-        },
-      )
-      if (type === 'success') {
-        Linking.openURL(newUrl)
-      }
-    }
-  }
-}
-
-Amplify.configure({
-  ...awsconfig,
-  oauth: {
-    ...awsconfig.oauth,
-    urlOpener,
-  },
-})
 
 const client = new QueryClient({
   defaultOptions: {
